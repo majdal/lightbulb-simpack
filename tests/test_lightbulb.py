d8bd9d36bfec5e61a15d753f026de85f400d0528
Majd Al-shihabi <majd.al@gmail.com>
2013-11-24 14:24:14 -0500

    Included the lightbulb simulation files directly in this repo

--- tests/test_lightbulb.py
+++ tests/test_lightbulb.py
@@ -1,20 +1,18 @@
 import unittest
+import sys
+from os.path import dirname, realpath
 
+cwd = dirname(dirname(dirname(realpath(__file__))))
+sys.path.append(cwd)
+
+import lightbulb_simpack
+from lightbulb_simpack.lightbulb.lightbulb import People, Lamps #, Intervention
+from lightbulb_simpack.state import State
+import garlicsim
 
 # Here's our "unit tests".
 class LightbulbCase(unittest.TestCase):
-
-    def test_init(self):
-        import sys
-        from os.path import dirname, realpath
-        cwd = dirname(dirname(dirname(realpath(__file__))))
-
-        sys.path.append(cwd)
-
-        from lightbulb.lightbulb import People, Lamps#, Intervention
-        import lightbulb_simpack.State
-        import garlicsim
-
+    def setUp(self):
         lamps = Lamps()
         people = People(250)
         interventions = []
@@ -24,9 +22,20 @@ class LightbulbCase(unittest.TestCase):
                 'LED': [],
                 'time': []
         }
-        state = lightbulb_simpack.State.create_root(Lamps, people, interventions, data)
-        garlicsim.simulate(state, 5)
+        self.state = State.create_root(lamps, people, interventions, data)        
 
+    def test_init(self):
+        garlicsim.simulate(self.state, 5)
+
+    def test_project(self):
+        project = garlicsim.Project(lightbulb_simpack)
+        root = project.root_this_state(self.state)
+        project.begin_crunching(root, 4)
+        print project.sync_crunchers()
+        print project.sync_crunchers()
+        print project.tree
+        (path,) = project.tree.all_possible_paths()
+        print path
 
 if __name__ == '__main__':
     unittest.main()
